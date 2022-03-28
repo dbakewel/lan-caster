55d1a163d5249d44db950f9284e016d70b4a23fb
Douglas Bakewell <dbakewel@gmail.com>
2022-03-28 16:22:15 -0700

    ray/reflector pseudo code

--- src/enginetest/maps/test21laser/servermap.py
+++ src/enginetest/maps/test21laser/servermap.py
@@ -8,41 +8,70 @@ import engine.geometry as geo
 
 
 class ServerMap(engine.servermap.ServerMap):
-    """LASER MECHANIC
+    """RAY MECHANIC
+
+    RayEmitter
+
+    RayReflector
 
         
     """
 
+    stepMapStartRayEmitter(self):
+        """RAY MECHANIC: Remove any polylines that are rays from last step """
 
-    """MIRROR MECHANIC
+    stepMapEndRayEmitter(self):
+        """RAY MECHANIC: compute ray beam and monster hits. """
 
-        
-    """
+        # find ray emitters
 
-    def initMirror(self):
-        """LASER MECHANIC: init method.
+        # trace ray beam
 
-        Set all mirrors to a random rotation to start.
+        # for each segment of ray, see if a monster intersets it.
+            # change monster to scull
+
+
+    rayTrace(self, x, y, r):
+        """Create line segment from x,y in direction r until something is intersected.
+
         """
-        self['startMirrorGID'] = False
-        for mirror in self.findObject(name="mirror", returnAll=True):
-            if self['startMirrorGID'] == False:
-                self['startMirrorGID'] = mirror['gid']
-            mirror['gid'] = random.randint(0, 7) + self['startMirrorGID']
+            # find all intersection points between objects and ray
 
-        self['nextMirrorRotateTime'] = time.perf_counter() + 1
+            # sort all intersections points from closest to x,y to farest
 
-    def mirrorAngle(self, mirror):
-        """ Given a mirror object, return the angle of the mirror based on its GID """
-        return (mirror['gid'] - self['startMirrorGID']) * 22.5
+            # walk intersection points until a stop or a reflector is found.
 
-    def stepSpriteStartMirror(self, sprite):
-        if sprite['name'] == "mirror" and self['nextMirrorRotateTime'] < time.perf_counter():
+            # create line segment
+
+            # if reflector is found then trace the reflection
+
+            # return line segment and any segments from reflection
+
+
+    def initRayReflector(self):
+        """RAY MECHANIC: init method.
+
+        Set all reflectors to a random rotation to start.
+        """
+        self['startReflectorGID'] = False
+        for reflector in self.findObject(name="reflector", returnAll=True):
+            if self['startReflectorGID'] == False:
+                self['startReflectorGID'] = reflector['gid']
+            reflector['gid'] = random.randint(0, 7) + self['startReflectorGID']
+
+        self['nextReflectorRotateTime'] = time.perf_counter() + 1
+
+    def stepSpriteStartRayReflector(self, sprite):
+        if sprite['name'] == "reflector" and self['nextReflectorRotateTime'] < time.perf_counter():
             sprite['gid'] += 1
-            if sprite['gid'] > self['startMirrorGID'] + 7:
-                sprite['gid'] = self['startMirrorGID']
+            if sprite['gid'] > self['startReflectorGID'] + 7:
+                sprite['gid'] = self['startReflectorGID']
 
-    def stepMapEndMirror(self):
-        if self['nextMirrorRotateTime'] < time.perf_counter():
-            self['nextMirrorRotateTime'] = time.perf_counter() + 1
+    def stepMapEndRayReflector(self):
+        if self['nextReflectorRotateTime'] < time.perf_counter():
+            self['nextReflectorRotateTime'] = time.perf_counter() + 1
             self.setMapChanged()
+
+    def getRayReflectorAngle(self, reflector):
+        """ Given a reflector object, return the angle of the reflector based on its GID """
+        return (reflector['gid'] - self['startReflectorGID']) * 22.5
