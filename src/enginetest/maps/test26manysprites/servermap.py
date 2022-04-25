815860e04b591fee2fb718ffaa09042374d007a9
Douglas Bakewell <dbakewel@gmail.com>
2022-04-24 19:04:08 -0700

    add many sprites test to enginetest

---
+++ src/enginetest/maps/test26manysprites/servermap.py
@@ -0,0 +1,18 @@
+"""ServerMap for Engine Test Map."""
+
+from engine.log import log
+import engine.servermap
+import engine.server
+
+
+class ServerMap(engine.servermap.ServerMap):
+    def stepMapStartChase(self):
+        """CHASE MECHANIC: stepMapStart method.
+
+        Have the monster chast the player.
+        """
+        for sprite in self['sprites']:
+            if sprite['type'] == "monster":
+                if "move" not in sprite:
+                    player = self.findObject(type="player")
+                    self.setMoveLinear(sprite, player['anchorX'], player['anchorY'], 10)
